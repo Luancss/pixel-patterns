094e8111c7281a5b9df732acbbb08f3ea747cba4
DiegoCrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2024-08-14 16:34:45 -0300

    feat: Add NotFound page

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Página não encontrada",
+  description:
+    "A página que você está tentando acessar não foi encontrada. Verifique o endereço e tente novamente.",
+};
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center gap-4">
+      <h1 className="text-6xl font-bold text-gray-100 slide-in-left">404</h1>
+      <p className="text-lg text-gray-300 slide-in-right">
+        Página não encontrada!
+      </p>
+    </div>
+  );
+}
