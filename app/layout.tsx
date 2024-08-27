1605b201c4e6c1f76d8e26bdc6015a0e941423c7
Luan <ventobo123@gmail.com>
2024-08-27 16:44:16 -0300

    Fixing error on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <Toaster />
         <div className="relative h-full w-full bg-black">
           <Background />
-          <div className="flex min-h-screen flex-col pb-20 ">
+          <div className="flex min-h-screen flex-col pb-20 box-border">
             <div className="flex flex-col gap-y-10 items-center text-center m-auto z-20 px-8">
               {children}
             </div>
