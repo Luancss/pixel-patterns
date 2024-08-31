3d8fb53c8755148b567d525b1c7fa3b5fcbe86f3
Laian <braumlaian@gmail.com>
2024-08-31 08:50:27 -0300

    fix: background: export it as object instead of default export, ensure all its imports are up to date

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import "./globals.css";
+
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "sonner";
-import Background from "@/components/layout/background";
+
+import { Background } from "@/components/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
