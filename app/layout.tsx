7da39e94b4a2e0ac0b714c5b27c9e13de525a161
Luan <ventobo123@gmail.com>
2024-09-01 22:50:46 -0300

    Adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,29 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { Background } from "@/components/layout";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pixel Patterns",
   description:
-    "O Pixel Patterns é um projeto voltado para desenvolvedores que visa simplificar a integração de componentes CSS e Tailwind em seus projetos.",
+    "Open-source collection of ready-to-use, reusable components built with Tailwind CSS. Simply copy and paste into your next project for seamless integration. Perfect for developers looking to enhance their workflow with high-quality, customizable UI snippets.",
+  openGraph: {
+    title: "Pixel Patterns",
+    description:
+      "Open-source collection of ready-to-use, reusable components built with Tailwind CSS. Simply copy and paste into your next project for seamless integration. Perfect for developers looking to enhance their workflow with high-quality, customizable UI snippets.",
+    url: "https://pixel-patterns-mauve.vercel.app/",
+    siteName: "Pixel Patterns",
+    locale: "pt-BR",
+    type: "website",
+    images: [
+      {
+        url: `https://i.pinimg.com/originals/c9/ce/49/c9ce490ba57ba69c66688381be6937aa.jpg`,
+        width: 1200,
+        height: 730,
+        alt: "Pixel Patterns",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -21,6 +37,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" />
+      </head>
       <body className={inter.className}>
         <Toaster />
         <div className="relative h-full w-full bg-black">
