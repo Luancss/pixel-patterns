8abe785414848c82249383d8f1bd854067b002a1
Luan <ventobo123@gmail.com>
2024-09-03 01:11:21 -0300

    feat: adding new components of buttons

--- app/buttons/(cards)/cards-buttons.tsx
+++ app/buttons/(cards)/cards-buttons.tsx
@@ -2373,6 +2373,225 @@ const CardsButton = () => {
         />
       }
     />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-l from-zinc-950"
+      content={
+        <button className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-50 px-6 font-medium text-neutral-950 transition active:scale-110 ">
+          Click me
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-l from-zinc-950"
+      content={
+        <button className="inline-flex h-12 items-center justify-center rounded-md bg-neutral-50 px-6 font-medium text-neutral-950 shadow-lg shadow-neutral-500/20 transition active:scale-95">
+          Click me
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-l from-zinc-600 to-zinc-950"
+      content={
+        <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-neutral-950 px-6 font-medium text-neutral-200 duration-500">
+          <div className="translate-x-0 transition group-hover:-translate-x-[150%]">
+            Hover me
+          </div>
+          <div className="absolute translate-x-[150%] transition group-hover:translate-x-0">
+            Hover me
+          </div>
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-r from-red-950"
+      content={
+        <button className="relative overflow-hidden rounded-md bg-red-700 px-5 py-2.5 text-white transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110">
+          Click me
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-t from-slate-950"
+      content={
+        <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-neutral-50 px-6 font-medium text-neutral-950">
+          <span>Hover me</span>
+          <div className="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+            >
+              <path
+                d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-b from-indigo-700/60"
+      content={
+        <button className="relative h-12 overflow-hidden rounded bg-neutral-950 px-5 py-2.5 text-white transition-all duration-200 hover:bg-neutral-800 hover:ring-offset-2 active:ring-2 active:ring-neutral-800">
+          Click me
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-r from-blue-700/30"
+      content={
+        <button
+          className="inline-flex h-12 items-center rounded-lg bg-blue-500 px-6 text-neutral-50 disabled:pointer-events-none disabled:opacity-50 "
+          disabled={true}
+        >
+          <div className="h-4 w-4 animate-spin rounded-full border-2 border-solid border-white border-t-transparent"></div>
+          <span className="ml-2">Loading... </span>
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#000"
+      information="Tailwind"
+      background="bg-[#fff]"
+      content={
+        <button
+          className="group relative inline-flex h-12 w-12 items-center justify-center overflow-hidden rounded-full bg-neutral-950"
+          title="Click"
+        >
+          <div className="transition duration-300 group-hover:rotate-[360deg]">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-neutral-200"
+            >
+              <path
+                d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-b from-gray-700/60 to-gray-900"
+      content={
+        <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-full border border-neutral-200 bg-transparent px-4 text-neutral-100">
+          <span className="relative inline-flex overflow-hidden">
+            <div className="absolute origin-bottom transition duration-500 [transform:translateX(-150%)_skewX(33deg)] group-hover:[transform:translateX(0)_skewX(0deg)]">
+              Hover me
+            </div>
+            <div className="transition duration-500 [transform:translateX(0%)_skewX(0deg)] group-hover:[transform:translateX(150%)_skewX(33deg)]">
+              Hover me
+            </div>
+          </span>
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-gradient-to-t from-slate-700/30"
+      content={
+        <button className="group relative h-12 rounded-full border border-neutral-200 bg-transparent px-4 text-neutral-100">
+          <span className="relative inline-flex overflow-hidden">
+            <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:-translate-y-[110%] group-hover:skew-y-12">
+              Hover me
+            </div>
+            <div className="absolute translate-y-[110%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">
+              Hover me
+            </div>
+          </span>
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-[#000]"
+      content={
+        <button className="group relative h-12 overflow-hidden rounded-[8px] border border-neutral-200 bg-transparent px-4 text-neutral-100">
+          <span className="relative inline-flex">
+            <span className="duration-700 [transition-delay:0.02s] group-hover:[transform:rotateY(360deg)]">
+              H
+            </span>
+            <span className="duration-700 [transition-delay:0.04s] group-hover:[transform:rotateY(360deg)]">
+              o
+            </span>
+            <span className="duration-700 [transition-delay:0.06s] group-hover:[transform:rotateY(360deg)]">
+              v
+            </span>
+            <span className="duration-700 [transition-delay:0.08s] group-hover:[transform:rotateY(360deg)]">
+              e
+            </span>
+            <span className="duration-700 [transition-delay:0.10s] group-hover:[transform:rotateY(360deg)]">
+              r
+            </span>
+            <span className="duration-700 [transition-delay:0.12s] group-hover:[transform:rotateY(360deg)]">
+              &nbsp;
+            </span>
+            <span className="duration-700 [transition-delay:0.14s] group-hover:[transform:rotateY(360deg)]">
+              m
+            </span>
+            <span className="duration-700 [transition-delay:0.16s] group-hover:[transform:rotateY(360deg)]">
+              e
+            </span>
+          </span>
+        </button>
+      }
+    />,
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Tailwind"
+      background="bg-[#000]"
+      content={
+        <button className="group relative">
+          <div className="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-300 transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3">
+            Hover me
+          </div>
+          <div className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5]"></div>
+        </button>
+      }
+    />,
   ];
 
   const filterCards = () => {
