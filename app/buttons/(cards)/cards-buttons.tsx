79b1c8b65f82f05337488555bc6cc1f2d1449bb0
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2024-09-07 13:48:33 -0300

    feat: button that changes position when clicked

--- app/buttons/(cards)/cards-buttons.tsx
+++ app/buttons/(cards)/cards-buttons.tsx
@@ -24,7 +24,44 @@ const CardsButton = () => {
   const generateKey = () => `card-${counter++}`;
 
   const cardsButton = [
-
+    <Card
+      key={generateKey()}
+      iconColor="#fff"
+      information="Button Example"
+      background="bg-gradient-to-r from-black-100 via-purple-100 to-pink-100"
+      content={
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+          <button type="button" class="uncommon-button-333">Clique Aqui</button>
+          <style>
+            .uncommon-button-333 {
+              padding: 10px 15px;
+              font-size: 1rem;
+              font-weight: bold;
+              color: #fff;
+              background: linear-gradient(135deg, #ff416c, #ff4b2b);
+              border: none;
+              border-radius: 50px;
+              cursor: pointer;
+              transition: transform 0.2s, background-color 0.4s;
+            }
+            
+            .uncommon-button-333:hover {
+              transform: rotate(5deg) scale(1.1);
+              background-color: #ff6200;
+            }
+            
+            .uncommon-button-333:active {
+              transform: scale(0.9) rotate(-5deg);
+            }
+          </style>
+          `
+          }}
+        />
+      }
+    />
+    ,
     <Card key={generateKey()}
       iconColor="#fff"
       information="css"
