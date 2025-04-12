697eb869e7e86d6bb931f3a481713072c4f24de9
Luan <ventobo123@gmail.com>
2025-04-11 21:05:17 -0300

    feat(home): add new feature button to navigate to code editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 import { ButtonEffect } from "@/components/button-effect";
-import { Button, FlipWords } from "@/components/ui";
 import { Footer, Navbar } from "@/components/layout";
 import { Cubic, Geminid, Star } from "@/components/shapes";
-import Spline from "@splinetool/react-spline/next";
+import { Button, FlipWords } from "@/components/ui";
+import { Sparkles } from "lucide-react";
 import { RamdonCards } from "./(ramdom-cards)/cards";
 
 export default function Home() {
@@ -17,9 +17,19 @@ export default function Home() {
       <Cubic />
       <div className="max-w-[600px] flex flex-col items-center justify-center gap-y-7 text-center">
         {/* <Spline scene="https://prod.spline.design/napPSAFnDzexMib0/scene.splinecode" /> */}
-        <Button className="bg-yellow-950/30 hover:bg-yellow-950/10 text-yellow-500 px-4 py-2 sm:px-6 sm:py-3">
-          <Link href="/contribute">Contribute to this project! 💡</Link>
-        </Button>
+        <div className="flex gap-4">
+          <Button className="bg-yellow-950/30 hover:bg-yellow-950/10 text-yellow-500 px-4 py-2 sm:px-6 sm:py-3">
+            <Link href="/contribute">Contribute to this project! 💡</Link>
+          </Button>
+
+          <Button className="bg-purple-950/30 hover:bg-purple-950/10 text-purple-500 px-4 py-2 sm:px-6 sm:py-3">
+            <Link href="/code-editor" className="flex gap-2 items-center">
+              <Sparkles className="w-4 h-4 text-white" />
+              New Feature
+            </Link>
+          </Button>
+        </div>
+
         <h1 className="text-4xl font-bold text-gray-100 slide-in-left lg:text-6xl">
           Collection of for development <FlipWords words={words} /> <br />
         </h1>
