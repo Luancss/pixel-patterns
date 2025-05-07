dbb9ce5b771fe3436a67e1b7848de73a129f5174
Luan <ventobo123@gmail.com>
2025-05-07 18:44:32 -0300

    refactor(app): move home page to dedicated folder and add layout component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,7 @@ import { Footer, Navbar } from "@/components/layout";
 import { Cubic, Geminid, Star } from "@/components/shapes";
 import { Button, FlipWords } from "@/components/ui";
 import { Sparkles } from "lucide-react";
-import { RandomCards } from "./(random-cards)/cards";
+import { RandomCards } from "./cards";
 
 export default function Home() {
   const words = ["enthusiast", "lovers", "fanatic", "admirer"];
